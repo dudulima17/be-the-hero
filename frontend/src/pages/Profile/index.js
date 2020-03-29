914b147414a5390b062ab030b57b88a98c69568d
Eduardo Lima <eduardolimaaraujo@hotmail.com>
2020-03-28 23:56:45 -0300

    Front End do App

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -36,13 +36,13 @@ export default function Profile() {
         try {
             await api.delete(`/incidents/${id}`, { 
                 headers: {
-                    'ong' : ongId
+                    Authorization: ongId,
                 }
             })
 
             setIncidents(incidents.filter(incident => incident.id !== id))
         } catch (error) {
-            alert('ERro ao deletar caso')
+            alert('Erro ao deletar caso, tente novamente.')
         }
 
     }
@@ -75,9 +75,9 @@ export default function Profile() {
                         <p>{incident.description}</p>
 
                         <strong>VALOR:</strong>
-                        <p>{Intl.NumberFormat("pt-BR", { style : "currency", "currency" : "BRL"}).format(incident.value)}</p>
+                        <p>{Intl.NumberFormat("pt-BR", { style : "currency", currency : "BRL"}).format(incident.value)}</p>
 
-                        <button type="button" onClick={() => handleDeleteIncident(incident.id)} >
+                        <button onClick={() => handleDeleteIncident(incident.id)} type="button" onClick={() => handleDeleteIncident(incident.id)} >
                             <FiTrash2 size={20} color="#a8a8b3" />
                         </button>
                     </li>    
